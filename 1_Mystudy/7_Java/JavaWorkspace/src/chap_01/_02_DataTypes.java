d47ede35db941431a807fcaf02bfd9c3b1aa7386
woojinrnd <woojin0342@gmail.com>
2023-10-24 00:07:40 +0900

    1023_01_Quiz

---
+++ 1_Mystudy/7_Java/JavaWorkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,20 @@
+package chap_01;
+
+public class _02_DataTypes
+{
+    //psvm
+    //main
+    public static void main(String[] args)
+    {
+        //sout
+        System.out.println("Hello World"); //string
+        System.out.println("안녕하세요");
+        System.out.println(12); //int
+        System.out.println(-34);
+        System.out.println(3.14); //float, double
+        System.out.println(true); //boolean
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+    }
+}
